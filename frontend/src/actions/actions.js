7bfb5f16c0a8b6159b99d3fce86fc83d059763d8
Prashobh <prashobh4657@gmail.com>
2024-09-14 11:26:02 +0530

    actions.js improved

--- frontend/src/actions/actions.js
+++ frontend/src/actions/actions.js
@@ -1,46 +1,22 @@
-// utils
-import { lS } from "../utils"
+import { lS } from "../utils";
+import { store } from "../redux/store";
+import { ActionTypes } from "./_types";
 
-// store
-import { store } from "../redux/store"
-
-// actions
-import { ActionTypes } from "./_types"
+const themeCycle = ['yellow', 'red', 'blue', 'green'];
 
 export const changeTheme = () => {
-    const theme = lS.get('theme');
-    if (theme) {
-        let color = 'yellow';
-        switch (theme) {
-            case 'yellow':
-                color = 'red';
-                break;
-            case 'red':
-                color = 'blue';
-                break;
-            case 'blue':
-                color = 'green';
-                break;
-            case 'green':
-                color = 'yellow';
-                break;
-            default:
-                break;
-        }
-        lS.set('theme', color);
-        store.dispatch({
-            type: ActionTypes.CHANGE_THEME,
-            payload: {
-                color: color
-            }
-        });
-    } else {
-        lS.set('theme', 'red');
-        store.dispatch({
-            type: ActionTypes.CHANGE_THEME,
-            payload: {
-                color: 'red'
-            }
-        });
-    }
+    const currentTheme = lS.get('theme') || 'red';
+    const nextTheme = getNextTheme(currentTheme);
+
+    lS.set('theme', nextTheme);
+    store.dispatch({
+        type: ActionTypes.CHANGE_THEME,
+        payload: { color: nextTheme }
+    });
+}
+
+const getNextTheme = (currentTheme) => {
+    const currentIndex = themeCycle.indexOf(currentTheme);
+    const nextIndex = (currentIndex + 1) % themeCycle.length;
+    return themeCycle[nextIndex];
 }
